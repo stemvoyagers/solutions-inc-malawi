8f3ca7ccf2f8927fa7a6e6e8b8ff78d19e99430f
coachemmy <coachemmyb@gmail.com>
2024-03-21 23:20:47 +0100

    identiko version 1.2

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["bootstrap/dist/css/bootstrap.min.css"]
-    }
-  }
 })
-
-
-
