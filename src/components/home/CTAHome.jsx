2b33f011477b0127b9258575ea4ef9370d2daa4b
coachemmy <coachemmyb@gmail.com>
2024-05-30 14:25:45 +0100

    enhancements

--- src/components/home/CTAHome.jsx
+++ src/components/home/CTAHome.jsx
@@ -10,16 +10,17 @@ const CTAHome = () => {
         <div className="h-screen overflow-hidden relative">
             <video className="h-40 md:h-auto bg:cover bg:center min-w-full min-h-full relative m-0 p-0" src={LandingVid} autoPlay loop muted alt="img1" />
 
-            <div className=" absolute md:z-50 text-[50%] md:text-[100%] mt-32 text-white w-full h-full md:top-0 flex items-center justify-center">
-                <p className="text-center text-shadow-lg"> Identiko Is Your One-Stop Shop <br />
-                                                            For Digital Identity, Verification,<br />
-                                                             And Technology Solutions</p>
+            <div className=" absolute md:z-50 text-[50%] md:text-[100%] ml-[6%] text-left mt-32 text-white w-full h-full md:top-0 flex items-center justify-start">
+                <div className="text-left text-shadow-lg">
+                <p> Identiko Is Your One-Stop Shop</p>
+                <p>For Digital Identity, Verification,</p>
+                <p> And Technology Solutions</p> </div>
 
 
                 <Link to="#section-1">
                     <div className="absolute bottom-24 left-0 right-0 mx-auto flex justify-center
                  items-center mb-20 text-white font-bold  rounded-full">
-                        <p className="bg-[#003997] rounded-full p-4 animate-bounce"><AiOutlineArrowDown/> </p>
+                        <p className="bg-[#003997] rounded-full p-4 animate-bounce"><AiOutlineArrowDown /> </p>
                     </div>
                 </Link>
             </div>
--- src/components/home/GallerySwipe.jsx
+++ src/components/home/GallerySwipe.jsx
@@ -2,7 +2,6 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { MyPhoto } from './MyPhoto'
-import { jideAgbaje } from '../../assets';
 import { useState, useEffect } from 'react';
 
 const GallerySwipe = () => {
@@ -31,7 +30,7 @@ const GallerySwipe = () => {
     dots: true,
     infinite: true,
     speed: 1000,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -40,14 +39,14 @@ const GallerySwipe = () => {
   return (
     <div className='bg-white overflow-x-hidden overflow-y-hidden flex items-center'>
       <div id="div1 h-[100%]"
-        className={`w-full md:w-1/2 bg-white transition-all duration-500 scroll-smooth ${isVisible1 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[100%]'
+        className={`w-full md:w-full bg-white transition-all duration-500 scroll-smooth ${isVisible1 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[100%]'
           }`}
       >
-        <h2 className=" text-center text-sm md:text-2xl font-bold text-black pt-12">OUR EVENT GALLERY</h2>
+        <h2 className=" text-center text-sm md:text-3xl font-bold text-black pt-12">Our Featured Works</h2>
         <Slider {...settings}>
           {MyPhoto.map((PhotoLink, index) => (
             <div key={index}>
-              <img src={PhotoLink} alt={`Slide ${index + 1}`} className="mx-auto p-3 flex flex-col md:flex-row md:grid md:w-[50%] h:[70%] mt-2 mb-12 rounded-3xl" />
+              <img src={PhotoLink} alt={`Slide ${index + 1}`} className="mx-auto p-3 flex flex-col md:flex-row md:grid md:w-[100%] h:[70%] mt-2 mb-12 rounded-3xl" />
             </div>
           ))}
         </Slider>
@@ -56,12 +55,8 @@ const GallerySwipe = () => {
         className={`hidden md:block w-1/2 bg-black transition-all duration-500  scroll-smooth ${isVisible2 ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[100%]'
           }`}
       >
-        <h2 className=" text-center text-sm md:text-2xl font-bold text-white pt-12">WORD FROM THE BOSS</h2>
-
-        <img src={jideAgbaje} alt='bossPic' className="mx-auto w-[50%] h:[50%] mt-2  rounded-full" />
-        <p className=" text-center text-xs md:text-smtext-sm font-bold text-white ">Just keep doing what you love, reward is near</p>
-        <p className=" text-center text-xs md:text-sm font-bold text-white mb-4">If you do not relent on your dreams, you will actualize it</p>
-
+      
+       
       </div>
     </div>
   );
