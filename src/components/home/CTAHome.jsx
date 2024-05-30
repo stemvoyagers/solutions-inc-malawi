c7b0f521635f9278581ead46ff1c5b0e331c8819
coachemmy <coachemmyb@gmail.com>
2024-05-30 12:17:56 +0100

    enhancements

--- src/components/home/CTAHome.jsx
+++ src/components/home/CTAHome.jsx
@@ -1,4 +1,5 @@
 
+import { AiOutlineArrowDown } from "react-icons/ai";
 import { LandingVid } from "../../assets";
 import { Link } from "react-router-dom";
 
@@ -9,16 +10,16 @@ const CTAHome = () => {
         <div className="h-screen overflow-hidden relative">
             <video className="h-40 md:h-auto w-screen min-w-full min-h-full relative m-0 p-0" src={LandingVid} autoPlay loop muted alt="img1" />
 
-            <div className=" absolute md:z-50 font-bold text-[50%] md:text-[300%] mb-2 text-white w-full h-full md:top-0 flex items-center justify-center">
+            <div className=" absolute md:z-50 font-bold text-[50%] md:text-[100%] mt-32 text-white w-full h-full md:top-0 flex items-center justify-center">
                 <p className="text-center text-shadow-lg"> Identiko Is Your One-Stop Shop <br />
                                                             For Digital Identity, Verification,<br />
                                                              And Technology Solutions</p>
 
 
                 <Link to="#section-1">
-                    <div className="absolute bottom-0 left-0 right-0 mx-auto flex justify-center
+                    <div className="absolute bottom-24 left-0 right-0 mx-auto flex justify-center
                  items-center mb-20 text-white font-bold  rounded-full">
-                        <p className="bg-[#003997] rounded-full p-4 animate-bounce">↡</p>
+                        <p className="bg-[#003997] rounded-full p-4 animate-bounce"><AiOutlineArrowDown/> </p>
                     </div>
                 </Link>
             </div>
