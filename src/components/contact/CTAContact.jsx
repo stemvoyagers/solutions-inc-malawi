1c3c33c58f3c1720a00d0d2b0f9f56ebc61c77d6
coachemmy <coachemmyb@gmail.com>
2024-05-30 10:37:40 +0100

    enhancements

--- src/components/contact/CTAContact.jsx
+++ src/components/contact/CTAContact.jsx
@@ -12,7 +12,7 @@ const CTAContact = () => {
 
             <div className="text-white max-w-screen-xl mx-auto mt-28 mb-28 z-10 ">
                 <div className="group gap-10 z-10 flex flex-col justify-center items-center text-center">
-                    <div className="text-[300%] md:mb-2 mt-40 font-bold ">
+                    <div className="text-[300%] md:mb-2 mt-40 font-bold text-shadow-lg ">
                         Contact Us  
                     </div>
                     <Link to="#section-1">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,22 @@ export default {
   theme: {
     extend: {
       colors:{white: "#ffffff"},
+      textShadow: {
+        'default': '0 2px 4px rgba(0, 0, 0, 0.10)',
+        'md': '0 3px 6px rgba(0, 0, 0, 0.15)',
+        'lg': '0 10px 15px rgba(0, 0, 0, 0.20)',
+        'xl': '0 20px 25px rgba(0, 0, 0, 0.25)',
+        '2xl': '0 25px 50px rgba(0, 0, 0, 0.25)',
+      },
+
       fontFamily: {
         sans: ["Helvetica Neue", "sans-serif"],      
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-textshadow'),
+
+  ],
 }
 
