fb766f027a0ff501b3d8f3d74bba7a3d43e66864
Ayomide <oyetimehin31@gmail.com>
2024-05-31 06:14:04 +0100

    changed the website logo

--- src/pages/Isms.jsx
+++ src/pages/Isms.jsx
@@ -1,30 +1,30 @@
-import CTAIsms from "../components/policies/CTAIsms"
-import IsmsBody from "../components/policies/IsmsBody"
-import { useState, useEffect } from "react"
-import { AiOutlineLoading } from "react-icons/ai"
-import { Spinner } from "react-bootstrap"
+import CTAIsms from "../components/policies/CTAIsms";
+import IsmsBody from "../components/policies/IsmsBody";
+import { useState, useEffect } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
+import { Spinner } from "react-bootstrap";
 
 const Isms = () => {
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-        setTimeout(()=>{
-          setIsLoading(false);
-        }, 2000)      
-      }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
 
-    return isLoading ? (
-        <div className="loading-state flex justify-center items-center min-h-screen" >
-            <Spinner animation="border" role="status">
-                <AiOutlineLoading className="animate-spin"/>
-            </Spinner>
-        </div>
-    ):(
-        <div>
-            <CTAIsms />
-            <IsmsBody />
-        </div>
-    )
-}
+  return isLoading ? (
+    <div className="loading-state flex justify-center items-center min-h-screen">
+      <Spinner animation="border" role="status">
+        <AiOutlineLoading className="animate-spin" />
+      </Spinner>
+    </div>
+  ) : (
+    <div>
+      <CTAIsms />
+      <IsmsBody />
+    </div>
+  );
+};
 
-export default Isms
+export default Isms;
