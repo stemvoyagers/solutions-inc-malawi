7d9bffb00695cba02b788f4c130fc5b26c9d7a79
coachemmy <coachemmyb@gmail.com>
2024-06-03 15:43:28 +0100

    revamp in progress

--- src/components/home/CTAHome.jsx
+++ src/components/home/CTAHome.jsx
@@ -16,24 +16,21 @@ const CTAHome = () => {
 
       <div className=" absolute md:z-50 text-[50%] md:text-[150%] text-left text-white w-full h-full md:top-0 flex items-end justify-start">
         <div className="ml-9 text-left text-shadow-lg mb-[15%]">
-          <p className="text-7xl mb-4">Identiko Integrated Solutions</p>
+          <p className="md:text-7xl mb-4">Identiko Integrated Solutions</p>
           <p> We Are Your One-Stop Shop For Digital </p>
           <p> Identity, Verification, And Technology Solutions</p>{" "}
         </div>
 
         <Link to="#section-1">
-        <div
-          className="absolute bottom-24 left-0 right-0 mx-auto hidden md:flex justify-center
-                 items-center text-white font-bold rounded-full"
-        >
-          <div className="rounded-full p-3 animate-bounce">
-            <img src={arrow_down} className="w-6 h-auto" />
+          <div
+            className="absolute bottom-24 left-0 right-0 mx-auto hidden md:flex justify-center
+            items-center text-white font-bold rounded-full">
+            <div className="rounded-full p-3 animate-bounce">
+              <img src={arrow_down} className="w-6 h-auto" />
+            </div>
           </div>
-        </div>
-      </Link>
+        </Link>
       </div>
-
-     
     </div>
   );
 };
--- src/components/home/GallerySwipe.jsx
+++ src/components/home/GallerySwipe.jsx
@@ -6,12 +6,12 @@ const GallerySwipe = () => {
   return (
     <div className=' overflow-x-hidden overflow-y-hidden flex flex-col items-center'>
      
-        <h2 className=" text-center text-xl md:text-3xl font-bold text-black mt-12 mb-12">Our Featured Works</h2>
+        <h2 className=" text-center text-xl md:text-3xl font-bold text-black mt-12 md:mb-12">Our Featured Works</h2>
      
-          <div className='flex flex-col md:flex-row justify-center items-center md:gap-20 mb-20'>
-            <img src ={five} alt = "fiveImg" className='w-[25%] rounded-md' />
-            <img src ={four} alt = "fourImg" className='w-[25%] rounded-md' />
-            <img src ={three} alt = "threeImg" className='w-[25%] rounded-md' />
+          <div className='flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20 mb-20'>
+            <img src ={five} alt = "fiveImg" className='w-[55%] md:w-[25%] rounded-md' />
+            <img src ={four} alt = "fourImg" className='w-[55%] md:w-[25%] rounded-md' />
+            <img src ={three} alt = "threeImg" className='w-[55%] md:w-[25%] rounded-md' />
           </div>
     </div>
   );
