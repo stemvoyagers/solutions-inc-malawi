77fe54109bb417c87cc2abb2421fba262ea91542
coachemmy <coachemmyb@gmail.com>
2024-06-24 13:50:04 +0100

    revamp in progress

--- src/components/services/CTAServices.jsx
+++ src/components/services/CTAServices.jsx
@@ -13,14 +13,14 @@ const CTAServices = () => {
     >
       <div className="text-white max-w-screen-xl mx-auto z-10  h-full md:top-0 flex justify-start items-end text-left">
         <div className="ml-9 text-left text-shadow-lg mb-[15%]">
-          <div className="hidden text-[50%] md:text-[150%]  md:mb-4 translate-y-72">
+          <div className="text-[50%] md:text-[150%]  md:mb-4 translate-y-20">
             Secure Your Future With Advanced Biometrics Solution
           </div>
         </div>
         
         <Link to="#section-1">
           <div
-            className="absolute top-[85%] left-0 right-0 mx-auto flex justify-center
+            className="absolute top-[80%] left-0 right-0 mx-auto flex justify-center
                  items-center text-white font-bold mt-1  rounded-full"
           >
             <div className="rounded-full p-3 animate-bounce">
