0796cf691e2db625cb67bf12ad1f257d1b5c15a0
Ayomide <oyetimehin31@gmail.com>
2024-09-12 15:51:14 +0100

    mr ope's changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+// import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [
     react(),
-    autoprefixer(),
+    // autoprefixer(),
     // tailwindcss('./tailwind.config.js'),
   ],
 })
